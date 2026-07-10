82c5a1dd79cd069ff95eea9dd468c56adaa895b2
gabjoao <joao.25.ribeiro01@gmail.com>
2026-07-09 22:35:35 -0300

    feat: React Router para roteamento das páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
 import SideBar from "./components/SideBar";
 import UserPage from "./pages/UserPage";
+import CursoPage from "./pages/CursoPage";
+import CategoryPage from "./pages/CategoryPage";
 
 function App() {
   return (
     <div className="flex flex-row">
       <SideBar />
-      <UserPage />
+      <Routes>
+        <Route>
+          <Route index element={<UserPage />} />
+          <Route path="cursos" element={<CursoPage />} />
+          <Route path="categorias" element={<CategoryPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
